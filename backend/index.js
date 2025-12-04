c3b898b62d6ddb459712f765b12fbbdce5bbccc9
Riya Rastogi <riya02rastogi@gmail.com>
2025-12-04 13:10:23 +0530

    added routes

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import taskRoutes from './routes/taskRoutes.js';
+import connectDB from './connection.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/tasks', taskRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Voice-Enabled Task Manager API is running');
+});
+
+// MongoDB Connection
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
