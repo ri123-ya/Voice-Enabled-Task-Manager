557661bc6826285aaafacf5f008d8b25e0f5582a
Riya Rastogi <riya02rastogi@gmail.com>
2025-12-04 12:53:42 +0530

    created mongoose schema

---
+++ backend/models/Task.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema({
+  title: { 
+    type: String, 
+    required: true 
+  },
+  description: { 
+    type: String 
+  },
+  status: { 
+    type: String, 
+    enum: ['To Do', 'In Progress', 'Done'], 
+    default: 'To Do' 
+  },
+  priority: { 
+    type: String, 
+    enum: ['Low', 'Medium', 'High', 'Critical'], 
+    default: 'Medium' 
+  },
+  dueDate: { 
+    type: Date 
+  },
+}, { 
+  timestamps: true 
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+export default Task;
