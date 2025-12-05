c09b52d62884a309793509995da86b567082ea3e
Riya Rastogi <riya02rastogi@gmail.com>
2025-12-05 08:35:16 +0530

    update the schema

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -15,7 +15,7 @@ const taskSchema = new mongoose.Schema({
   },
   priority: { 
     type: String, 
-    enum: ['Low', 'Medium', 'High', 'Critical'], 
+    enum: ['Urgent', 'High Priority', 'Low Priority', 'Critical'], 
     default: 'Medium' 
   },
   dueDate: { 
