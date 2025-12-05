3aa2a5f96f979612b38062e7c1fa20346a81b47c
Riya Rastogi <riya02rastogi@gmail.com>
2025-12-05 09:40:15 +0530

    added edit on task card

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const TaskCard = ({ task, onDelete, onStatusChange }) => {
+const TaskCard = ({ task, onDelete, onStatusChange, onEdit }) => {
   const priorityColors = {
     'Critical': 'bg-red-500',
     'Urgent': 'bg-orange-500',
@@ -26,15 +26,28 @@ const TaskCard = ({ task, onDelete, onStatusChange }) => {
         <span className={`${priorityColor} text-white text-xs font-semibold px-3 py-1 rounded-full`}>
           {task.priority}
         </span>
-        <button
-          onClick={() => onDelete(task._id)}
-          className="opacity-0 group-hover:opacity-100 transition-opacity duration-200 text-gray-500 hover:text-red-400"
-          aria-label="Delete task"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
-          </svg>
-        </button>
+        <div className="flex gap-2">
+          {/* Edit Button */}
+          <button
+            onClick={() => onEdit(task)}
+            className="opacity-0 group-hover:opacity-100 transition-opacity duration-200 text-gray-500 hover:text-blue-400"
+            aria-label="Edit task"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+            </svg>
+          </button>
+          {/* Delete Button */}
+          <button
+            onClick={() => onDelete(task._id)}
+            className="opacity-0 group-hover:opacity-100 transition-opacity duration-200 text-gray-500 hover:text-red-400"
+            aria-label="Delete task"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+            </svg>
+          </button>
+        </div>
       </div>
 
       {/* Task Title */}
