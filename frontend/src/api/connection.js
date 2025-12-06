ea96dc8d83922864a1ae5f161a82ee261704a47c
Riya Rastogi <riya02rastogi@gmail.com>
2025-12-06 17:08:14 +0530

    added backend hosted url

--- frontend/src/api/connection.js
+++ frontend/src/api/connection.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api/tasks';
+const API_URL = 'https://aerchain-backend.onrender.com/api/tasks';
 
 export const connection = {
   // Get all tasks
